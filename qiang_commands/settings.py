9b93b0e563d02466a74ebe5d4a967ddf903d6346
zhangjingqiang <zhangjingqiang@gmail.com>
2014-10-08 23:48:44 +0900

    Add STATIC_ROOT

--- qiang_commands/settings.py
+++ qiang_commands/settings.py
@@ -95,8 +95,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
