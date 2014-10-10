30f16361f1ac47b8fced3d156a4fef7b8a00aea0
zhangjingqiang <zhangjingqiang@gmail.com>
2014-10-10 22:08:46 +0900

    Removed static admin url

--- qiang_commands/urls.py
+++ qiang_commands/urls.py
@@ -1,14 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings
 
 urlpatterns = patterns('',
-        url(r'^$', include('qiang.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-        )
-
-urlpatterns += patterns('',
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.STATIC_ROOT,
-            }),
-        )
+    url(r'^$', include('qiang.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
